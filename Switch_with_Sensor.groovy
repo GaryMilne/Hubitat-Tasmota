87f1afadcc0f07e2426c2effe5724e216b8c1253
GaryMilne <garyjmilne@gmail.com>
2022-05-18 21:43:34 -0500

    Add files via upload

--- Switch_with_Sensor.groovy
+++ Switch_with_Sensor.groovy
@@ -46,7 +46,7 @@ import groovy.transform.Field
 @Field static final sensor2UnitSuffix = ['TEMPERATURE' : 'Degrees', 'HUMIDITY' : '% RH', 'DEWPOINT' : 'Degrees', 'ILLUMINANCE' : 'lux']
 
 metadata {
-		definition (name: "Tasmota Sync - Switch with Sensor", namespace: "garyjmilne", author: "Gary J. Milne", importUrl: "https://raw.githubusercontent.com/GaryMilne/Hubitat-Tasmota/main/Tasmota%20Sync%20Switch%20with%20sensor%20-%20Current%20Version.txt", singleThreaded: true )  {
+		definition (name: "Tasmota Sync - Switch with Sensor", namespace: "garyjmilne", author: "Gary J. Milne", importUrl: "https://github.com/GaryMilne/Hubitat-Tasmota/blob/main/Switch_with_Sensor.groovy", singleThreaded: true )  {
         //capability "AirQuality"
         //capability "CarbonDioxideMeasurement"
         //capability "IlluminanceMeasurement"
